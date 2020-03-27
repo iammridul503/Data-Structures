e3d02f597ef9bcbfb0617a4190958bc52a6e931e
iammridul503 <iammridul503.it@gmail.com>
2020-03-28 00:18:33 +0530

    6.Stack Implementation using collection

---
+++ Stack3.java
@@ -0,0 +1,32 @@
+/*
+@author:"Mridul Mishra"
+Title:"Stack Implementation using Java collections"
+**folows LIFO(Last In First Out)/FILO(First In Last Out)
+*/
+import java.util.*;  
+public class Stack3
+{  
+    public static void main(String args[])
+    {  
+        Stack<String> stack = new Stack<String>();//declaring a stack  
+
+        stack.push("Data");  
+        stack.push("Structures");  
+        stack.push("and");  
+        stack.push("Algorithms");  
+        stack.push("in");
+        stack.push("Java"); 
+        Iterator<String> itr=stack.iterator();  
+        while(itr.hasNext())
+        {  
+        System.out.print(itr.next()+" ");  
+        } 
+        System.out.println(); 
+
+        stack.pop();   
+        for(String str:stack)
+        {  
+        System.out.print(str+" ");  
+        }  
+    }  
+}  
